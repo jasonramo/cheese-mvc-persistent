bb7a84170dde371227ac06d0674f277545e763b0
jasonramo <jasonaramo@gmail.com>
2018-12-05 15:56:21 -0600

    Created an index handler inside CategoryController and corresponding index.html for category. passed all category data through handler to retrieve the list of categories. created <ul> for the category names.

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -3,12 +3,24 @@ package org.launchcode.controllers;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("category")
+@RequestMapping(value="category")
 public class CategoryController {
 
     @Autowired
     private CategoryDao categoryDao;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "Categories");
+        return "/category/index";
+    }
+
+
 }
