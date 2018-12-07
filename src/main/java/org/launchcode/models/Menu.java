5e33f3b389e4461c490175f9d66aa7b1c3ceaefb
jasonramo <jasonaramo@gmail.com>
2018-12-06 18:36:26 -0600

    Created a Menu class to allow our cheeses to be placed in.

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,57 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+
+@Entity
+public class Menu {
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToMany
+    private ArrayList<Cheese> cheeses = new ArrayList<>();
+
+    public Menu(String name){
+        this.name = name;
+    }
+
+    public Menu(){
+
+    }
+
+    public void addItem(Cheese item) {
+        //TODO 1 - what to put here???
+        cheeses.add(item);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Cheese> getCheeses() {
+        return cheeses;
+    }
+}
