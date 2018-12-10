b16f61620440e497e6f965b479e7667b25d5e0ec
jasonramo <jasonaramo@gmail.com>
2018-12-10 14:42:14 -0600

    added the ability to create new menus by creating the add handler method for both get and post requests as well as setting up the form to accept the new menu.

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,12 +1,17 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "menu")
@@ -26,4 +31,20 @@ public class MenuController {
         return "/menu/index";
     }
 
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title","Create New Menu");
+        model.addAttribute(new Menu());
+        return "/menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create New Menu");
+            return "/menu/add";
+        }
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
+    }
 }
