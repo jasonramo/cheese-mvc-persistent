066b0bb3ff7bd2652d293ca42319941767eae4c3
jasonramo <jasonaramo@gmail.com>
2018-12-10 15:54:34 -0600

    updated the fragments.html file to display per the request

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Cheese;
 import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
@@ -66,5 +67,23 @@ public class MenuController {
         model.addAttribute("title","Add item to menu: " + menu.getName());
         return "/menu/add-item";
     }
+
+    @RequestMapping(value="add-item", method=RequestMethod.POST)
+    public String addItem(Model model, @ModelAttribute @Valid AddMenuItemForm form, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("form", form);
+            return "menu/add-item";
+        }
+        Cheese theCheese = cheeseDao.findOne(form.getCheeseId());
+        Menu theMenu = menuDao.findOne(form.getMenuId());
+        theMenu.addItem(theCheese);
+        menuDao.save(theMenu);
+
+        return "redirect:/menu/view/" + theMenu.getId();
+
+
+    }
+
 
 }
